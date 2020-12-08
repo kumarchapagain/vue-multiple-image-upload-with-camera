374207b8b7d544c642c4b9927a6c9348341484dc
Kumar Chapagain <kumarsamip03sc@gmail.com>
2020-12-08 17:25:00 +0545

    vue multiple image upload with camera initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# multiple-image-upload
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <div style="display: flex; justify-content: center;">
+      <v-camera
+          @upload-success="uploadImageSuccess"
+          @edit-image="editImage"
+          @mark-is-primary="markIsPrimary"
+          @limit-exceeded="limitExceeded"
+          @before-remove="beforeRemove"
+          id-upload="myIdUpload"
+          edit-upload="myIdEdit"
+          :max-image=3
+          :video-height=200
+          :video-width= 190
+          :canvas-height= 337.5
+          :canvas-width= 450
+          :camera=true
+          primary-text=""
+          browse-text="Browse picture(s)"
+          drag-text="Drag pictures"
+          drop-text="Drop your file here ..."
+          mark-is-primary-text="Set as default"
+          popup-text=""
+          :multiple=true
+          :show-edit=true
+          :show-delete=true
+          :show-add=true
+      ></v-camera>
+    </div>
+  </div>
+</template>
+
+<script>
+import MultipleImageUpload from './components/MultipleImageUpload.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VCamera: MultipleImageUpload
+  },
+  methods: {
+    uploadImageSuccess(formData, index, fileList) {
+      console.log('data', formData, index, fileList)
+      // Upload image api
+      // axios.post('http://your-url-upload', formData).then(response => {
+      //   console.log(response)
+      // })
+    },
+    beforeRemove(index, removeCallBack) {
+      console.log('index', index)
+      var r = confirm("remove image")
+      if (r == true) {
+        removeCallBack()
+      } else {
+      }
+    },
+    editImage(formData, index, fileList) {
+      console.log('edit data', formData, index, fileList)
+    },
+    markIsPrimary(index, fileList){
+      console.log('markIsPrimary data', index, fileList)
+    },
+    limitExceeded(amount){
+      console.log('limitExceeded data', amount)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
